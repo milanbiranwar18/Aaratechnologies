0a89e4f7ec7ebf9b2369c70c2d62ea3b1de785ff
Milan Biranwar <milanbiranwar06@gmail.com>
2024-05-07 10:56:04 +0530

    [Task] Refactored code

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import GoogleLogin, FacebookLogin
 
 urlpatterns = [
     path('user_registration/', views.Registration.as_view(), name='registration'),
